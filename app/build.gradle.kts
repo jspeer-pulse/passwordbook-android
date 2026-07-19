a9c4d71b7b8b4e2534abac2ebb4f7272784b38b1
a <a@a.a>
2026-07-19 19:31:25 +0800

    fix: 修正 versionCode 为 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.example.passwordbook"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
+        versionCode = 5
         versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
